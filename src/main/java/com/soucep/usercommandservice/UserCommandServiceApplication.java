95399707ed9716fa63cbc805b2e03bbfc28eaf56
Zsolt_Jakab <Zsolt_Jakab@epam.com>
2017-07-03 16:15:31 +0200

    gradle setup added

---
+++ src/main/java/com/soucep/usercommandservice/UserCommandServiceApplication.java
@@ -0,0 +1,12 @@
+package com.soucep.usercommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCommandServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/soucep/usercommandservice/UserCommandServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soucep.usercommandservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserCommandServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
